82fd31d213c563d205ee067e94152a0ddd507a1a
JeffOwOSun <sunyushi9546@gmail.com>
2016-02-10 17:49:22 -0500

    Complete cropping functionality for one image

--- image-crop.js
+++ image-crop.js
@@ -7,7 +7,7 @@ let imagefile="000000.JPEG";
 let annotatefile="000000.xml";
 
 //read the image
-let imPromise = Jimp.read(imagefile)
+let imPromise = Jimp.read(imagefile);
 
 //read the xml
 let parser = new xml2js.Parser();
@@ -18,12 +18,9 @@ let xmlPromise = new Promise((resolve, reject)=>{
         resolve(data);
     });
 }).then(data=>{
-    console.log(data);
     return new Promise((resolve, reject)=>{
         parser.parseString(data, (err, result)=>{
             if (err) reject(err);
-            console.dir(result);
-            console.log('xml parsing Done');
             resolve(result);
         });
     });
@@ -31,9 +28,23 @@ let xmlPromise = new Promise((resolve, reject)=>{
 
 //wait until both are finished
 Promise.all([imPromise, xmlPromise]).then(res=>{
-    let [im, xml]=res;
+    let im = res[0],
+        xml = res[1];
     //console.log(res);
-    console.log('All done!');
+    xml.annotation.object.forEach(obj=>{
+      obj.bndbox.forEach(bndbox=>{
+        let box = [
+          bndbox.xmin[0],
+          bndbox.ymin[0],
+          bndbox.xmax[0]-bndbox.xmin[0],
+          bndbox.ymax[0]-bndbox.ymin[0],
+        ].map(Number);
+        im.clone()
+          .crop(...box)
+          .resize(256, 256)
+          .write('000000cropped.JPEG');
+      });
+    });
 }).catch(err=>{
-    console.log(err);
+    console.error(err);
 });
